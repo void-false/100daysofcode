3950e2e918cd840f72aaf8620d2ef010d64c2b9f
void-false <void.false@gmail.com>
2019-10-09 23:44:57 +0300

    High Score

---
+++ crawling_spider/HighScore.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_Back_pressed() -> void:
+	get_tree().change_scene("res://MainMenu.tscn")
--- crawling_spider/MainMenu.gd
+++ crawling_spider/MainMenu.gd
@@ -1,9 +1,13 @@
 extends Node
 
 
-func _on_Exit_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_Play_pressed() -> void:
 	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_HighScore_pressed() -> void:
+	get_tree().change_scene("res://HighScore.tscn")
+
+
+func _on_Exit_pressed() -> void:
+	get_tree().quit()
