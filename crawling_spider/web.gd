9ee735699dd6ec02e304e06e296577170fb47071
void-false <void.false@gmail.com>
2019-10-30 23:51:01 +0200

    🐛

--- crawling_spider/web.gd
+++ crawling_spider/web.gd
@@ -6,4 +6,5 @@ var webdir : Vector2 = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	var collide_object : KinematicCollision2D = move_and_collide(Vector2(speed * delta * webdir.x, speed * delta * webdir.y))
 	if collide_object:
+		collide_object.collider.queue_free()
 		queue_free()
