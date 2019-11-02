89e9d096c136b64321fd559fadd7ab4e4305cd6d
void-false <void.false@gmail.com>
2019-11-02 02:57:53 +0200

    Fixed killing TileMap

--- crawling_spider/web.gd
+++ crawling_spider/web.gd
@@ -6,5 +6,10 @@ var webdir : Vector2 = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	var collide_object : KinematicCollision2D = move_and_collide(Vector2(speed * delta * webdir.x, speed * delta * webdir.y))
 	if collide_object:
-		collide_object.collider.queue_free()
 		queue_free()
+		if collide_object.collider.name == "TileMap":
+			return 
+		else: 
+			collide_object.collider.queue_free()
+		
+		
