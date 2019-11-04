57072a2078a5be3a0842175d121fb28f3e110571
void-false <void.false@gmail.com>
2019-11-05 01:00:11 +0200

    Raycast

--- crawling_spider/Player.gd
+++ crawling_spider/Player.gd
@@ -201,6 +201,14 @@ func movement_loop() -> void:
 	if not is_alive:
 		free_fall = true
 
+	if motion.x > 0:
+		$RayCast2D.position.x = -25
+	elif motion.x < 0:
+		$RayCast2D.position.x = 25
+		
+	if not $RayCast2D.is_colliding():
+		motion.x = 0
+
 	move_and_slide(motion * SPEED + gravity, Vector2.UP)
 
 	$Ceiling.visible = is_on_ceiling()
