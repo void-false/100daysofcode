aaa4fe3b5389fd870076113c4324d6ba5df438e0
void-false <void.false@gmail.com>
2019-11-11 01:49:34 +0200

    more work on movement

--- movement/Player.gd
+++ movement/Player.gd
@@ -9,22 +9,19 @@ const SPEED : int = 150
 
 func _physics_process(delta: float) -> void:
 	control_loop()
-	movement_loop(delta)
 	collision_loop()
-	print(gravity)
+	movement_loop(delta)
 		
 func control_loop() -> void:
 	movement.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	movement.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
-func movement_loop(delta: float) -> void:
-	move_and_slide(movement * SPEED + gravity)
 	
 func collision_loop() -> void:
 	free_fall = true
 	for raycast in raycast_list:
 		if raycast.is_colliding(): 
 			free_fall = false
+			break
 	if free_fall:
 		movement.y = 0
 		gravity.x = 0
@@ -34,7 +31,14 @@ func collision_loop() -> void:
 			gravity = Vector2.UP
 		elif $EastDown.is_colliding() or $EastUp.is_colliding(): # right wall
 			gravity = Vector2.RIGHT
-		elif $SouthLeft.is_colliding() or $SouthRight.is_colliding(): # floor
-			gravity = Vector2.DOWN
 		elif $WestDown.is_colliding() or $WestUp.is_colliding(): # left wall
 			gravity = Vector2.LEFT
+		elif $SouthLeft.is_colliding() or $SouthRight.is_colliding(): # floor
+			gravity = Vector2.DOWN
+			movement.y = clamp(movement.y, 0, 1)
+			
+func movement_loop(delta: float) -> void:
+	if not movement and gravity.length() < 5:
+		pass
+	else:
+		move_and_slide(movement * SPEED + gravity)
