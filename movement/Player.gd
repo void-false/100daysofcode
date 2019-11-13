41cf8d2ac3329474664268cdfb4195fc2589290f
void-false <void.false@gmail.com>
2019-11-14 01:48:04 +0200

    movement

--- movement/Player.gd
+++ movement/Player.gd
@@ -19,30 +19,27 @@ func control_loop() -> void:
 func collision_loop() -> void:
 	free_fall = false
 
-	if $NorthLeft.is_colliding() or $NorthRight.is_colliding(): # ceiling
-		gravity = Vector2.UP
+	if $NorthLeft.is_colliding() and $NorthRight.is_colliding(): # ceiling
+		gravity = Vector2.ZERO
+	elif $NorthLeft.is_colliding() or $NorthRight.is_colliding(): # up right or left cliff
+		gravity = Vector2.UP * SPEED
 	elif $EastDown.is_colliding() or $EastUp.is_colliding(): # right wall
-		gravity = Vector2.RIGHT
+		gravity = Vector2.ZERO
+
 	elif $WestDown.is_colliding() or $WestUp.is_colliding(): # left wall
-		gravity = Vector2.LEFT
+		gravity = Vector2.ZERO
+
 	elif $SouthLeft.is_colliding() or $SouthRight.is_colliding(): # floor
-		gravity = Vector2.DOWN
+		gravity = Vector2.ZERO
 		movement.y = clamp(movement.y, 0, 1)
 	else:
 		free_fall = true
 	
-	if not free_fall and not is_on_wall():
-		gravity *= 50
-		print("HERE", gravity)
-	
 	if free_fall:
 		movement.y = 0
-		gravity.x = 0
 		gravity.y += 10
 
 func movement_loop(delta: float) -> void:
 	move_and_slide(movement * SPEED + gravity)
-#	if not movement and gravity.length() < 5:
-#		pass
-#	else:
-#		move_and_slide(movement * SPEED + gravity)
+
+
