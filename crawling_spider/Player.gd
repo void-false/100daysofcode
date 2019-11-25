3f58dfe56dad050b2921e51d92488b9228ace6ea
void-false <void.false@gmail.com>
2019-11-25 02:06:47 +0200

    implementing new movement into main game

--- crawling_spider/Player.gd
+++ crawling_spider/Player.gd
@@ -3,33 +3,107 @@ extends KinematicBody2D
 onready var web = preload("res://web.tscn")
 
 const JUMP_HEIGHT = -600
-const SPEED = 150
+const SPEED : int = 200
+const ACCEL : int = 130
 
-var gravity = Vector2.UP
-var motion = Vector2()
+var gravity : Vector2 = Vector2.ZERO
+var movement : Vector2 = Vector2.ZERO
 var movedir = "right"
-var free_fall = false
+var free_fall : bool = false
 var previous_plane
 var is_alive = true
 var is_dead_anim_playing = false
-var is_grabbing : bool = false
-var boost : int = 2
 
 func _physics_process(delta):
 	control_loop()
+	collision_loop()
 	movement_loop()
 	spritedir_loop()
-	check_collisions_loop()
+	#check_collisions_loop()
+	
+func control_loop():
+	if not is_alive:
+		return
+	if not visible:
+		movement = Vector2.ZERO
+		return
+
+	movement.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	movement.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		
+func collision_loop() -> void:
+	if is_alive:
+		free_fall = false
+	
+	if $NorthLeft.is_colliding() and $WestUp.is_colliding() and not $WestDown.is_colliding():
+		if movement.y < 0:
+			gravity = Vector2.UP * ACCEL + Vector2.LEFT
+		elif movement.x < 0:
+			gravity = Vector2.LEFT * ACCEL + Vector2.UP
+		else:
+			gravity = (Vector2.UP + Vector2.LEFT) * ACCEL
+	
+	elif $NorthRight.is_colliding() and $EastUp.is_colliding() and not $EastDown.is_colliding():
+		if movement.y < 0:
+			gravity = Vector2.UP * ACCEL + Vector2.RIGHT
+		elif movement.x > 0:
+			gravity = Vector2.RIGHT * ACCEL + Vector2.UP
+		else:
+			gravity = (Vector2.UP + Vector2.RIGHT) * ACCEL
+
+	elif $SouthRight.is_colliding() and $EastDown.is_colliding() and not $SouthLeft.is_colliding():
+		if movement.y > 0:
+			gravity = Vector2.DOWN * ACCEL + Vector2.RIGHT
+		elif movement.x > 0:
+			gravity = Vector2.RIGHT * ACCEL + Vector2.DOWN
+		else:
+			gravity = (Vector2.DOWN + Vector2.RIGHT) * ACCEL
+
+	elif $SouthLeft.is_colliding() and $WestDown.is_colliding() and not $SouthRight.is_colliding():
+		if movement.y > 0:
+			gravity = Vector2.DOWN  * ACCEL + Vector2.LEFT
+		elif movement.x < 0:
+			gravity = Vector2.LEFT  * ACCEL + Vector2.DOWN
+		else:
+			gravity = (Vector2.DOWN + Vector2.LEFT) * ACCEL
+	
+	
+	elif $NorthLeft.is_colliding() or $NorthRight.is_colliding():
+		gravity = Vector2.UP * ACCEL
+	elif $EastDown.is_colliding() or $EastUp.is_colliding():
+		gravity = Vector2.RIGHT * ACCEL
+	elif $WestUp.is_colliding() or $WestDown.is_colliding():
+		gravity = Vector2.LEFT * ACCEL
+	elif $SouthRight.is_colliding() or $SouthLeft.is_colliding():
+		gravity = Vector2.DOWN * ACCEL
+		movement.y = clamp(movement.y, 0, 1)
+	else:
+		free_fall = true
+		
+	if free_fall:
+		gravity.x = 0
+		gravity.y = abs(gravity.y) + 20
+		movement.y = 0
+
+func movement_loop() -> void:
+
+	if free_fall or not is_on_wall():
+		move_and_slide(movement * SPEED + gravity)
+	elif movement.length() > 0:
+		move_and_slide(movement * SPEED + gravity)
+
+	if not is_alive:
+		free_fall = true
 	
 func spritedir_loop():
 	if not is_alive:
 		if not is_dead_anim_playing:
 			$Sprite/AnimationPlayer.play("Dead")
 			is_dead_anim_playing = true
-			motion = Vector2.ZERO
+			movement = Vector2.ZERO
 		return
-	
-	match motion:
+
+	match movement:
 		Vector2.UP:
 			movedir = "up"	
 			if gravity == Vector2.LEFT:
@@ -110,8 +184,8 @@ func spritedir_loop():
 				$Sprite.rotation_degrees = -90
 		Vector2.ZERO:
 			$Sprite/AnimationPlayer.play("Idle")
-			
-			
+
+
 	if is_on_ceiling(): # and (movedir == "left" or movedir == "right"):
 		if previous_plane == "wall_left":
 			$Sprite.flip_h = true
@@ -122,97 +196,13 @@ func spritedir_loop():
 	elif is_on_wall(): # and (movedir == "up" or movedir == "down"):
 		$Sprite.flip_v = false
 		if previous_plane == "ceiling":
-			if motion.x > 0:
+			if movement.x > 0:
 				$Sprite.flip_h = false
-			elif motion.x < 0:
+			elif movement.x < 0:
 				$Sprite.flip_h = true
 	elif is_on_floor() or free_fall:
 		$Sprite.flip_v = false
 		$Sprite.rotation_degrees = 0
-		
-			
-func control_loop():
-	if not is_alive:
-		return
-	if not visible:
-		motion = Vector2.ZERO
-		return
-
-	is_grabbing = Input.is_action_pressed("grab")
-
-	motion.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	motion.y =  Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
-	if Input.is_action_just_pressed("fire"):
-		var web_instance = web.instance()
-		if movedir == "left":
-			web_instance.position = Vector2(position.x - 64, position.y)
-			web_instance.webdir = Vector2.LEFT
-		elif movedir == "right":
-			web_instance.position = Vector2(position.x + 64, position.y)
-			web_instance.webdir = Vector2.RIGHT
-		elif movedir == "up":
-			web_instance.position = Vector2(position.x, position.y - 64)
-			web_instance.webdir = Vector2.UP
-		elif movedir == "down":
-			web_instance.position = Vector2(position.x, position.y + 64)
-			web_instance.webdir = Vector2.DOWN
-		get_tree().get_root().add_child(web_instance)
-
-func movement_loop() -> void:
-	
-	if free_fall:
-		gravity.y += 20
-	
-	if is_on_ceiling() and is_alive:
-		free_fall = false
-		gravity = Vector2.UP
-		if is_grabbing:
-			motion.y = clamp(motion.y, -1, 0)
-	elif is_on_wall():
-		free_fall = false
-		if test_move(transform, Vector2.LEFT):
-			gravity = Vector2.LEFT
-			if is_grabbing:
-				motion.x = clamp(motion.x, -1, 0)
-		elif test_move(transform, Vector2.RIGHT):
-			gravity = Vector2.RIGHT
-			if is_grabbing:
-				motion.x = clamp(motion.x, 0, 1)
-	elif is_on_floor():
-		free_fall = false
-		motion.y = 0
-		gravity = Vector2.DOWN
-	else:
-		motion.y = 0
-		free_fall = true
-	
-	if free_fall and previous_plane == "wall_right" and motion.x == 0:
-		motion.x = boost
-	elif free_fall and previous_plane == "wall_left" and motion.x == 0:
-		motion.x = -boost
-			
-			
-	if is_grabbing and free_fall and previous_plane == "ceiling":
-		move_and_slide(Vector2.UP, Vector2.UP)
-		return
-	previous_plane = get_previous_plane()
-
-	if not is_alive:
-		free_fall = true
-		
-	if $RayCast1.is_colliding() or $RayCast2.is_colliding():
-		free_fall = false
-#		gravity *= 5
-
-	move_and_slide(motion * SPEED + gravity, Vector2.UP)
-
-	$Ceiling.visible = is_on_ceiling()
-	$WallLeft.visible = is_on_wall() and gravity == Vector2.LEFT
-	$WallRight.visible = is_on_wall() and gravity == Vector2.RIGHT
-	$Floor.visible = is_on_floor()
-	$Grabbing.visible = is_grabbing
-	
 	
 func get_previous_plane():
 	if is_on_ceiling():
@@ -246,4 +236,5 @@ func _on_ExitDoor_body_entered(body):
 	$"../CanvasLayer/Fireworks1".emitting = true
 	$"../CanvasLayer/Fireworks2".emitting = true
 	Globals.end_time = OS.get_ticks_msec()
+	
 	
