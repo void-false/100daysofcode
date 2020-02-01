06391a082192955d61bea0ac6377490f67817300
void-false <void.false@gmail.com>
2020-02-01 23:20:58 +0200

    Hello Godot

---
+++ hello_godot32/Label.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.connect("timeout", self, "_on_Timer_timeout")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Timer_timeout() -> void:
+	$Sprite.visible = !$Sprite.visible
